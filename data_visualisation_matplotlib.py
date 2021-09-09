e94f332201aca1468b95e45dd97a376452b77065
ash-yizhen <88515457+ash-yizhen@users.noreply.github.com>
2021-09-10 07:38:07 +1000

    Update data_visualisation_matplotlib.py

--- data_visualisation_matplotlib.py
+++ data_visualisation_matplotlib.py
@@ -1,1 +1,6 @@
+import matplotlib.pyplot as plt 
+fig, ax = plt.subplots() #fig object is container that holds everything you see on page; ax is part of the page hold the data
+plt.show()
 
+ax.plot(seattle_weather["MONTH", seattle_weather["MLY-TAVG-NORMAL"])
+plt.show()
