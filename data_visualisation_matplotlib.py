552465a60ed609f771ce75e9c36a96a747488048
ash-yizhen <88515457+ash-yizhen@users.noreply.github.com>
2021-09-19 08:47:20 +1000

    Update data_visualisation_matplotlib.py

--- data_visualisation_matplotlib.py
+++ data_visualisation_matplotlib.py
@@ -11,6 +11,28 @@ ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-TAVG-NORMAL"])
 ax.plot(austin_weather["MONTH"], austin_weather["MLY-TAVG-NORMAL"])
 plt.show()
 
-#Customise your plot                        
+#Customise your plot   
+#marker - change the marker in the plot
+#linestyle -- change the line style in the plot
+#color -- changet the line of the color
+ax.plot(austin_weather["MONTH"], austin_weather["MLY-TAVG-NORMAL"], marker = "v", linestyle="--", color="r")
+#maker is v shape, line is dashed, line color is red
+ax.set_xlabel("Time(months)")
+ax.set_ylabel("Average temperature (Fahrenheit degrees)")                 
+ax.set_title("Weather in Seattle")
+
+#adding data
+#small multiples -- arrange on the page as a grid with rows and columns
+fig, ax = plt.subplots()
+fig, ax = plt.subplots(3,2)
+plt.show()
+ax.shape  #return result (3,2) 
+#add data in the little subplot [0,0]
+ax[0,0].plot(seattle_weather["MONTH"], seattle_weather["MLK-PRCP-NORMAL"], color = 'b')
+plt.show()
+                        
+                        
+
+
                         
                         
